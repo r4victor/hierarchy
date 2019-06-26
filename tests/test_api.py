3766331a86f02d6fbc168e14170e4c30142398cf
Victor Skvortsov <vds003@gmail.com>
2019-06-26 14:21:59 +0500

    Add tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,31 +1,24 @@
 import pytest
 
-from tests.test_data import populate_table_with_example_rows, example_tree
+from tests.test_data import example_hierarchy
 
 
-def test_create_item(client, app):
-    with app.app_context():
-        populate_table_with_example_rows()
+def test_create_item(client, app, example_hierarchy):
     response = client.post('/item', json={'name': 'test item', 'parent_id': None})
     assert response.status_code == 201
     assert response.headers['Location']
 
 
-def test_replace_item(client, app):
-    with app.app_context():
-        populate_table_with_example_rows()
+def test_replace_item(client, app, example_hierarchy):
     updated_item = {'id': 5, 'name': 'test item', 'parent_id': 3}
     response = client.put('/item', json=updated_item)
     assert response.get_json() == updated_item
 
 
-def test_get_item(client, app, example_tree):
-    with app.app_context():
-        populate_table_with_example_rows()
-    
+def test_get_item(client, app, example_hierarchy):
     response = client.get('/item/2')
     data = response.get_json()
-    item = example_tree['children'][0]
+    item = example_hierarchy[0]
     assert data == {
         'id': item['id'],
         'name': item['name'],
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,88 +3,115 @@ import pytest
 from app.db import get_db_conn
 from app.models import Node
 
-from tests.test_data import populate_table_with_example_rows, example_tree
+from tests.test_data import example_hierarchy
 
 
-def test_root_node(app):
+def test_get_root_node(app):
     expected_root_node = Node(id=1, name='root', parent_id=None, lft=0, rgt=1)
     with app.app_context():
-        root_node = Node.get_by_id(1)
+        root_node = Node.get_root_node()
     
     assert root_node == expected_root_node
 
 
-def test_create_node(app):
-    expected_root_node = Node(id=1, name='root', parent_id=None, lft=0, rgt=3)
-    expected_new_node = Node(id=2, name='new node', parent_id=1, lft=1, rgt=2, tree_id=2)
+def test_get_hierarchy(app, example_hierarchy):
     with app.app_context():
-        new_node = Node.create(expected_new_node.name, Node.get_by_id(expected_new_node.parent_id))
-        assert new_node == Node.get_by_id(expected_new_node.id)
-        root_node = Node.get_by_id(expected_root_node.id)
-
-    assert root_node == expected_root_node and new_node == expected_new_node
+        hierarchy = Node.get_hierarchy()
+    assert hierarchy == example_hierarchy
 
 
-def test_get_subtree(app, example_tree):
+def test_delete_node(app, example_hierarchy):
+    del example_hierarchy[1]
     with app.app_context():
-        populate_table_with_example_rows()
-        node = Node.get_by_id(1)
-        subtree = node.get_subtree()
-    assert subtree == example_tree
-
-
-def test_delete_node(app, example_tree):
-    del example_tree['children'][1]
-    with app.app_context():
-        populate_table_with_example_rows()
         Node.get_by_id(5).delete()
-        tree = Node.get_by_id(1).get_subtree()
-    assert tree == example_tree
+        hierarchy = Node.get_hierarchy()
+    assert hierarchy == example_hierarchy
 
 
-def test_move(app, example_tree):
-    subtree = example_tree['children'].pop(1)
-    subtree['parent_id'] = 2
-    example_tree['children'][0]['children'].append(subtree)
-    with app.app_context():
-        populate_table_with_example_rows()
-        Node.get_by_id(5).move(Node.get_by_id(2))
-        tree = Node.get_by_id(1).get_subtree()
-    
-    assert tree['children'] == example_tree['children']
-
-
-@pytest.mark.parametrize(
-    'name', ['Simple', 'немного harder', '234 234 444 ', 'path/to/something', 'try_underscores__']
-)
-def test_name_validation_valid(name):
-    try:
-        Node.validate_name(name)
-    except ValueError:
-        pytest.fail()
-
-
-@pytest.mark.parametrize(
-    'name', ['', ' ', '\t', '  ', ' 123', 're  re', '😀']
-)
-def test_name_validation_invalid(name):
-    with pytest.raises(ValueError):
-        Node.validate_name(name)
-
-
-def test_name_uniqueness_fail(app):
-    with app.app_context():
-        node = Node.create('Unique', Node.get_by_id(Node.get_root_id()))
-        with pytest.raises(ValueError):
-                Node.create('Unique', node)
-
-
-def test_name_uniqueness_accept(app):
-    with app.app_context():
-        Node.create('Unique', Node.get_by_id(Node.get_root_id()))
+class TestNameValidation:
+    @pytest.mark.parametrize(
+        'name', ['Simple', 'немного harder', '234 234 444 ', 'path/to/something', 'try_underscores__']
+    )
+    def test_name_validation_valid(self, name):
         try:
-            Node.create('Unique', Node.get_by_id(Node.get_root_id()))
+            Node.validate_name(name)
         except ValueError:
             pytest.fail()
+
+
+    @pytest.mark.parametrize(
+        'name', ['', ' ', '\t', '  ', ' 123', 're  re', '😀']
+    )
+    def test_name_validation_invalid(self, name):
+        with pytest.raises(ValueError):
+            Node.validate_name(name)
+
+
+class TestCreateNode:
+    def test_create_node(self, app):
+        expected_root_node = Node(id=1, name='root', parent_id=None, lft=0, rgt=3)
+        expected_new_node = Node(id=2, name='new node', parent_id=1, lft=1, rgt=2, tree_id=2)
+        with app.app_context():
+            new_node = Node.create(expected_new_node.name, Node.get_by_id(expected_new_node.parent_id))
+            assert new_node == Node.get_by_id(expected_new_node.id)
+            root_node = Node.get_by_id(expected_root_node.id)
+
+        assert root_node == expected_root_node and new_node == expected_new_node
+
+    def test_name_uniqueness_fail(self, app):
+        with app.app_context():
+            node = Node.create('Unique', Node.get_root_node())
+            with pytest.raises(ValueError):
+                    Node.create('Unique', node)
+
+    def test_name_uniqueness_accept(self, app):
+        with app.app_context():
+            Node.create('Unique', Node.get_root_node())
+            try:
+                Node.create('Unique', Node.get_root_node())
+            except ValueError:
+                pytest.fail()
+
+
+class TestMove:
+    def test_move_rigth(self, app, example_hierarchy):
+        item = example_hierarchy[0]['children'].pop(0)
+        item['parent_id'] = 7
+        example_hierarchy[2]['children'].append(item)
+        with app.app_context():
+            Node.get_by_id(3).move(Node.get_by_id(7))
+            hierarchy = Node.get_hierarchy()
+        
+        assert hierarchy == example_hierarchy
+
+    def test_move_subtree(self, app, example_hierarchy):
+        subtree = example_hierarchy.pop(1)
+        subtree['parent_id'] = 2
+        example_hierarchy[0]['children'].append(subtree)
+        with app.app_context():
+            Node.get_by_id(5).move(Node.get_by_id(2))
+            hierarchy = Node.get_hierarchy()
+        
+        assert hierarchy == example_hierarchy
+
+    def test_move_under_itself(self, app, example_hierarchy):
+        with app.app_context():
+            with pytest.raises(ValueError):
+                Node.get_by_id(3).move(Node.get_by_id(3))
+
+    def test_move_under_child(self, app, example_hierarchy):
+        with app.app_context():
+            with pytest.raises(ValueError):
+                Node.get_by_id(2).move(Node.get_by_id(3))
+    
+    def test_keep_name_unique(self, app, example_hierarchy):
+        with app.app_context():
+            node3 = Node.get_by_id(3)
+            node7 = Node.get_by_id(7)
+            Node.create(name='I am unique within my tree', parent_node=node3)
+            Node.create(name='I am unique within my tree', parent_node=node7)
+            with pytest.raises(ValueError):
+                node3.move(node7)
+
 
         
